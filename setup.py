0aa93603ccde9adf8a382385636a28c84f73a310
Samuel Rohaut <srohaut@gmail.com>
2023-09-04 22:37:56 +0200

    fix: bump version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ here = Path(__file__).resolve().parent
 
 setup(
     name=name,
-    version='1.20',
+    version='1.21',
     description='Program to run plugins to inhibit system '
     'sleep/suspend/hibernate',
     long_description=here.joinpath('README.md').read_text(),
